9d76b57b1435703e661efa8969221085ffe77865
nonducorduco311-cyber <nonducorduco311@gmail.com>
2026-06-23 19:01:52 +0000

    Remove stale Run-Scanner.bat launcher

--- Run-Scanner.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-title ByTE X Bit Posture Scanner
-"%~dp0bxb-scan.exe" --html "%~dp0posture-report.html"
-echo.
-echo A copy of this report was saved next to this file as posture-report.html
-echo.
-pause
