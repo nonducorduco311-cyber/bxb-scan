d9567807849ae7c4e282ce60add54047a179cf30
nonducorduco311-cyber <nonducorduco311@gmail.com>
2026-06-23 18:12:42 +0000

    Remove unused double-click detection files

--- dblclick_other.go
+++
@@ -1,7 +0,0 @@
-//go:build !windows
-
-package main
-
-// On Linux/macOS the program runs in the terminal the user invoked it from,
-// so there is no vanishing-window problem and no need to pause.
-func launchedByDoubleClick() bool { return false }
